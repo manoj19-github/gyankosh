c2ce1d12664b46ce0215a87ca75dd907af4c43f4
manoj santra <santramanoj1997@gmail.com>
2024-03-02 22:12:42 +0530

    started

--- app/about/page.tsx
+++
@@ -1,9 +0,0 @@
-import { FC } from "react"
-
-
-const AboutPage:FC = ()=>{
-    return<></>
-}
-
-
-export default AboutPage
--- app/context/initialState.ts
+++
@@ -1,5 +0,0 @@
-import GlobalInterface from "./glolbalInterface";
-
-export const initialState:GlobalInterface={
-    theme:"dark"
-}
